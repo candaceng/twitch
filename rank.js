d29245b96d5a3e635548e2d72ab97bd4e18850fe
Candace Ng <50592521+candaceng@users.noreply.github.com>
2024-05-31 09:30:12 -0400

    test

--- rank.js
+++ rank.js
@@ -1,19 +1,19 @@
 ((
+  /** @type {string} */ streamUptimeString,
+  /** @type {string} */ streamStartDateString,
   /** @type {string} */ urlEncodedGetMmrHistoryResponseJson,
+  /** @type {string} */ playerName,
 ) => {
 
   const getMmrHistoryResponseJson = decodeURIComponent(urlEncodedGetMmrHistoryResponseJson);
-  if (/^Error Connecting To Remote Server\b/i.test(getMmrHistoryResponseJson)) {
-    return 'API is down, try again later';
-  }
-
+  
   try {
     const response = JSON.parse(getMmrHistoryResponseJson);
     const data = response['data'];
-    
+
     return `${data['currenttierpatched']} (${data['ranking_in_tier']} RR)`;
   } catch (e) {
     return 'something went wrong with the API oop';
   }
-  
+
 })
