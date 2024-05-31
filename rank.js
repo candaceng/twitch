69ef9494f6ca057b5d563f55baa872ad81c5fd29
Candace Ng <50592521+candaceng@users.noreply.github.com>
2024-05-31 08:53:24 -0400

    test

--- rank.js
+++ rank.js
@@ -1,8 +1,5 @@
 ((
-  /** @type {string} */ streamUptimeString,
-  /** @type {string} */ streamStartDateString,
   /** @type {string} */ urlEncodedGetMmrHistoryResponseJson,
-  /** @type {string} */ playerName,
 ) => {
 
   const getMmrHistoryResponseJson = decodeURIComponent(urlEncodedGetMmrHistoryResponseJson);
@@ -13,10 +10,8 @@
   try {
     const response = JSON.parse(getMmrHistoryResponseJson);
     const data = response['data'];
-
-    let rank = `${data['currenttierpatched']} ${data['ranking_in_tier']} RR`;
-
-    return `${playerName} is ${rank}`;
+    
+    return `${data['currenttierpatched']} (${data['ranking_in_tier']} RR)`;
   } catch (e) {
     return 'something went wrong with the API oop';
   }
