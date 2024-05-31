cf7cdd513bf45fc0423e75f4533f419c1714d98e
Candace Ng <50592521+candaceng@users.noreply.github.com>
2024-05-31 08:31:48 -0400

    test

--- rank.js
+++ rank.js
@@ -1,21 +1,22 @@
 ((
-  urlEncodedResponseJson,
+  // urlEncodedResponseJson,
   playerName,
 ) => {
 
-  const responseJson = decodeURIComponent(urlEncodedResponseJson);
-  if (/^Error Connecting To Remote Server\b/i.test(responseJson)) {
-    return 'API is down, try again later';
-  }
+  // const responseJson = decodeURIComponent(urlEncodedResponseJson);
+  // if (/^Error Connecting To Remote Server\b/i.test(responseJson)) {
+  //   return 'API is down, try again later';
+  // }
 
-  try {
-    const response = JSON.parse(responseJson);
-    const data = response['data'];
+  // try {
+  //   const response = JSON.parse(responseJson);
+  //   const data = response['data'];
     
-    let rank = `${data['currenttierpatched']} ${data['ranking_in_tier']} RR`;
+  //   let rank = `${data['currenttierpatched']} ${data['ranking_in_tier']} RR`;
 
-    return `${playerName} is ${rank}`;
-  } catch (e) {
-    return 'something went wrong with the API oop';
-  }
+  //   return `${playerName} is ${rank}`;
+  // } catch (e) {
+  //   return 'something went wrong with the API oop';
+  // }
+  return `${playerName} test`;
 })
