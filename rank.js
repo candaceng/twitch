e53e505d27fe60d334a118dcfd31203482ddeccc
Candace Ng <50592521+candaceng@users.noreply.github.com>
2024-06-01 13:11:27 -0400

    error handling

--- rank.js
+++ rank.js
@@ -4,14 +4,16 @@
 ) => {
 
   const getMmrHistoryResponseJson = decodeURIComponent(urlEncodedGetMmrHistoryResponseJson);
+  errorMsg = 'API is down, try again later';
   
   try {
     const response = JSON.parse(getMmrHistoryResponseJson);
+    if (response['status'] != 200) return errorMsg;
     const data = response['data'];
 
     return `${data['currenttierpatched']} (${data['ranking_in_tier']} RR)`;
   } catch (e) {
-    return 'something went wrong with the API oop';
+    return errorMsg;
   }
 
 })
